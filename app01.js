c3ad7a4a97c4c2fc1f56399a24cc64c3ea5cbf9d
Erick de la Cruz <erickdanieldelacruz@gmail.com>
2026-03-17 01:38:05 -0400

    agrego funcion countSpecialNumbers y primeros test automatizados

--- app01.js
+++ app01.js
@@ -1,10 +1,31 @@
-let numbers = [3, 8, 15, 20, 7];
-let count = 0;
-for (let i = 0; i < numbers.length; i++) {
-    let num = numbers[i];
-    if (num % 2 === 0 && num > 10) {
-         console.log("numero especial");
-    } else {
-        console.log("numero normal");
+function countSpecialNumbers(numbers){
+    let specialCount = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        let num = numbers[i];
+        if (num % 2 === 0 && num > 10) {
+            specialCount++;
+        }
     }
-} 
+    return specialCount;
+}
+
+let numbers = [3, 8, 15, 20, 7];
+let result = countSpecialNumbers(numbers);
+let expected = 1;
+
+if (result === expected) {
+    console.log("Test 1: PASS");
+} else {
+    console.log("Test 1: FAIL");
+}
+
+let numbers2 = [2, 4, 12, 14];
+let result2 =
+countSpecialNumbers(numbers2);
+let expected2 = 2;
+if (result2 === expected2) {
+    console.log("Test 2: PASS");
+} else {
+    console.log("Test 2: FAIL");
+}
