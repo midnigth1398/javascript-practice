37a89b80f3ee80161c33a0ff966dd1ce84c53677
Erick de la Cruz <erickdanieldelacruz@gmail.com>
2026-03-19 15:17:40 -0400

    feat: agrego sistema de dinero, upgrades y oleadas

--- game.js
+++ game.js
@@ -1,31 +1,50 @@
 let enemyHealth = 10;
+let money = 0;
+let damage = 1;
+
 function updateUI() {
     document.getElementById("enemy").textContent =
             "vida del enemigo: " + enemyHealth;
+    
+            document.getElementById("money").textContent =
+            "Dinero: " + money;
 }
 
 function attack() {
     let damage = 2;
     enemyHealth = enemyHealth - damage;
-
     updateUI();
 
     if (enemyHealth <= 0) {
-
-        document.getElementById("enemy").textContent = "🎉 Ganaste!";
-        clearInterval(gameLoop);
+        money += 5
+        nextEnemy();
     }
 }
 
 function enemyAttack() {
     enemyHealth--;
-
     updateUI();
 
     if (enemyHealth <= 0) {
-        document.getElementById("enemy").textContent = "🎉 Ganaste!";
-        clearInterval(gameLoop);
+        money += 5;
+        nextEnemy();
     }
+}
+
+function nextEnemy() {
+    enemyHealth = 10;
+    updateUI();
+}
+
+function upgrade() {
+    if (money >= 10) {
+    money -= 10;
+    damage++
+    console.log(" Dano mejorado:", damage);
+    updateUI();
+    } else {
+    console.log("✖️ No tienes suficiente dinero");
+}
 }
 let gameLopp = setInterval(enemyAttack, 2000);
 
